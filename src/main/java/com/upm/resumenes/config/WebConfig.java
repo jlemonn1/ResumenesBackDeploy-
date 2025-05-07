749310f3b263a45d8a273392c5a04412b60fe30d
jlemonn1 <javierlimon6@gmail.com>
2025-05-07 22:53:05 +0200

    first commit

--- src/main/java/com/upm/resumenes/config/WebConfig.java
+++ src/main/java/com/upm/resumenes/config/WebConfig.java
@@ -9,11 +9,8 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
-        // Esta ruta captura cualquier ruta no estática y la redirige al frontend
-        registry.addViewController("/{path:^(?!api|swagger-ui|v3|index\\.html|static|assets).*$}")
-                .setViewName("forward:/index.html");
-
-        registry.addViewController("/**/{path:^(?!.*\\..*$).*$}")
+        // Captura rutas como /profile, /buscar, /login, etc.
+        registry.addViewController("/{path:[^\\.]*}")
                 .setViewName("forward:/index.html");
     }
 }
